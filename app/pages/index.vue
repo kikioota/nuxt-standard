c89cc50a6791faeb45380e020c15b089a96ad6d9
takamine-hbk <takamine@fvs-net.co.jp>
2026-04-30 08:54:57 +0900

    会社PCアップ

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,13 +11,11 @@ useSeoMeta({
 
 <template>
   <p class="pt-[3rem] md:pt-[8rem] bg-gray-dddddd text-white font-sans">vueテンプレート</p>
-  <p class="">TOPページ</p>
+  <p class="pb-[3rem]">TOPページ</p>
 
   <HeadingPrimary enLabel="about" className="mb-[5rem] lg:mb-[10rem]">見出しメイン</HeadingPrimary>
 
   <HeadingPrimary tag="div" titleTag="h3" enLabel="about" className="mb-[5rem] lg:mb-[10rem]">見出しメイン</HeadingPrimary>
-
-  <img src="/assets/images/sample/400_320_102400-0.jpg" width="247" height="320" alt="">
 
   <Image type="jpg" src="/_nuxt/assets/images/sample/img-about" width="500" height="300" alt="サンプル画像" :lazy="false" className="mx-auto" />
 
